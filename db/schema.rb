2861d8a307cfd3155e43b26c376aa0a7a93f4468
Austin Mason <austinpmason@gmail.com>
2017-01-12 10:02:17 +0000

    create event model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170110145721) do
+ActiveRecord::Schema.define(version: 20170112100138) do
+
+  create_table "events", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
