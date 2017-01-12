55fbf366856cbee41bf8e1578be7c75e00e14623
Austin Mason <austinpmason@gmail.com>
2017-01-12 09:57:55 +0000

    create session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,18 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user
+      session[:current_user_id] = user.id
+      redirect_to user_path(user)
+    else
+      flash.now[:danger] = 'Invalid email'
+      render 'new'
+    end
   end
 
   def destroy
+    session.delete(:current_user_id)
+    redirect_to root_path
   end
 end
